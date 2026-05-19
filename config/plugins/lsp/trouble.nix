c3e5db5fdf6a4ab17ff779471fa484cd7e35ecb8
Elliot Luque Pascual <git@elliotluque.com>
2026-05-19 13:07:16 +0200

    feat: keymaps trouble.nvim

--- config/plugins/lsp/trouble.nix
+++ config/plugins/lsp/trouble.nix
@@ -1,23 +1,67 @@
+{ ... }:
 {
   plugins.trouble = {
-  enable = true;
+    enable = true;
 
-  settings = {
-    auto_close = true;
-    auto_open = false;
-    auto_preview = true;
-    focus = false;
+    settings = {
+      auto_close = true;
+      auto_open = false;
+      auto_preview = true;
+      focus = false;
 
-    follow = true;
-    indent_guides = true;
+      follow = true;
+      indent_guides = true;
 
-    multiline = true;
-    warn_no_results = false;
+      multiline = true;
+      warn_no_results = false;
+      open_no_results = false;
 
-    open_no_results = false;
+      use_diagnostic_signs = true;
 
-    # UI
-    use_diagnostic_signs = true;
+      modes = {
+        diagnostics = {
+          win = {
+            position = "bottom";
+            size = 10;
+          };
+        };
+      };
+    };
   };
- };
+
+  extraConfigLua = ''
+    -- Toggle global diagnostics
+    vim.keymap.set("n", "<leader>xx", "<cmd>Trouble diagnostics toggle<cr>", {
+      desc = "Diagnostics (Trouble)",
+    })
+
+    -- Buffer diagnostics
+    vim.keymap.set("n", "<leader>xX", "<cmd>Trouble diagnostics toggle filter.buf=0<cr>", {
+      desc = "Buffer Diagnostics (Trouble)",
+    })
+
+    -- Quickfix
+    vim.keymap.set("n", "<leader>xq", "<cmd>Trouble qflist toggle<cr>", {
+      desc = "Quickfix (Trouble)",
+    })
+
+    -- Location list
+    vim.keymap.set("n", "<leader>xl", "<cmd>Trouble loclist toggle<cr>", {
+      desc = "Location List (Trouble)",
+    })
+
+    -- LSP references in Trouble
+    vim.keymap.set("n", "gr", "<cmd>Trouble lsp_references toggle<cr>", {
+      desc = "References (Trouble)",
+    })
+
+    -- Navegación dentro de Trouble
+    vim.keymap.set("n", "]t", function()
+      require("trouble").next({ skip_groups = true, jump = true })
+    end)
+
+    vim.keymap.set("n", "[t", function()
+      require("trouble").prev({ skip_groups = true, jump = true })
+    end)
+  '';
 }
