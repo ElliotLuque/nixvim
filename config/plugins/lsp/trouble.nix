c4e4ba89ff4277b120dec437e8d2c90a833a08bd
Elliot Luque Pascual <git@elliotluque.com>
2026-05-19 13:05:29 +0200

    feat: trouble.nvim config

--- config/plugins/lsp/trouble.nix
+++ config/plugins/lsp/trouble.nix
@@ -1,10 +1,23 @@
 {
   plugins.trouble = {
-    enable = true;
-  
-    settings = {
-      auto_close = true;
-      use_diagnostic_signs = true;
-    };
+  enable = true;
+
+  settings = {
+    auto_close = true;
+    auto_open = false;
+    auto_preview = true;
+    focus = false;
+
+    follow = true;
+    indent_guides = true;
+
+    multiline = true;
+    warn_no_results = false;
+
+    open_no_results = false;
+
+    # UI
+    use_diagnostic_signs = true;
   };
+ };
 }
