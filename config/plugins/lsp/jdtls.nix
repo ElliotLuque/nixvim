dd1d2b4e2f2662cf9594cc517656889780b9d152
Elliot Luque Pascual <git@elliotluque.com>
2026-05-19 11:34:02 +0200

    feat: jdtls nvim plugin

---
+++ config/plugins/lsp/jdtls.nix
@@ -0,0 +1,35 @@
+{
+  plugins.jdtls = {
+    enable = true;
+
+    settings = {
+      java = {
+        format = {
+          enabled = true;
+        };
+        
+        completion = {
+          favoriteStaticMembers = [
+            "org.hamcrest.MatcherAssert.assertThat"
+            "org.hamcrest.Matchers.*"
+            "org.junit.Assert.*"
+            "java.util.Objects.requireNonNull"
+          ];
+        };
+
+        sources = {
+          organizeImports = {
+            starThreshold = 9999;
+            staticStarThreshold = 9999;
+          };
+        };
+
+        codeGeneration = {
+          toString = {
+            template = "\${object.className}{\${member.name()}=\${member.value}, \${otherMembers}}";
+          };
+        };
+      };
+    };
+  };
+}
